ca45a6ec6f0a8a0d38838c39282e89b04d8a2104
cslee <cslee0034@gmail.com>
2023-02-14 03:00:25 +0900

    fix: Import TypeORM repository to usersModule
    TypeORM repository를 usersModule에 넣어주었습니다.

--- Portfolio_maker_back/src/users/users.module.ts
+++ Portfolio_maker_back/src/users/users.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersController } from './users.controller';
+import { UsersEntity } from './users.entitiy';
 import { UsersService } from './users.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([UsersEntity])],
   controllers: [UsersController],
   providers: [UsersService],
 })
