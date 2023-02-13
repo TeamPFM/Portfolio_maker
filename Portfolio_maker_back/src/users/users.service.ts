b28609774a73b99ad827335e3d2accf63b85e017
cslee <cslee0034@gmail.com>
2023-02-14 02:56:55 +0900

    chore: Add bcrypt
    bcrypt를 추가했습니다.

--- Portfolio_maker_back/src/users/users.service.ts
+++ Portfolio_maker_back/src/users/users.service.ts
@@ -3,6 +3,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { CreateUsersDto } from './dto/create-user.dto';
 import { UsersEntity } from './users.entitiy';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UsersService {
