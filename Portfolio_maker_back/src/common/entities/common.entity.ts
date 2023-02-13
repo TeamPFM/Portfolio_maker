6c15dd1310472a881a5ecd9d37be0ad4f2ef9a65
cslee <cslee0034@gmail.com>
2023-02-14 02:44:39 +0900

    fix: fix id UUID to number
    id의 타입을 UUID에서 number로 바꿨습니다.

--- Portfolio_maker_back/src/common/entities/common.entity.ts
+++ Portfolio_maker_back/src/common/entities/common.entity.ts
@@ -4,13 +4,11 @@ import {
   PrimaryGeneratedColumn,
   UpdateDateColumn,
 } from 'typeorm';
-import { IsUUID } from 'class-validator';
 import { Exclude } from 'class-transformer';
 
 export abstract class CommonEntity {
-  @IsUUID()
-  @PrimaryGeneratedColumn('uuid')
-  id: string;
+  @PrimaryGeneratedColumn()
+  id: number;
 
   @CreateDateColumn({
     type: 'timestamp',
