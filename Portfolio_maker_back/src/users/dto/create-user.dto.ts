b2d6cf2fdd457547f6b0d45c1443412ad25a32b9
cslee <cslee0034@gmail.com>
2023-02-15 04:07:58 +0900

    feat: Add dto

--- Portfolio_maker_back/src/users/dto/create-user.dto.ts
+++ Portfolio_maker_back/src/users/dto/create-user.dto.ts
@@ -2,14 +2,14 @@ import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
 
 export class CreateUsersDto {
   @IsEmail()
-  @IsNotEmpty()
+  @IsNotEmpty({ message: '이메일은 비워져 있을 수 없습니다.' })
   email: string;
 
   @IsString()
-  @IsNotEmpty()
+  @IsNotEmpty({ message: '이은 비워져 있을 수 없습니다.' })
   name: string;
 
   @IsString()
-  @IsNotEmpty()
+  @IsNotEmpty({ message: '비밀번호는 비워져 있을 수 없습니다.' })
   password: string;
 }
