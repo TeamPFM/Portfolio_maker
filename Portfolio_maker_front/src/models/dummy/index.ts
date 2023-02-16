bd8eedcfc69c153426b10237907520d192b00874
Gihun <563710@naver.com>
2023-02-16 18:42:52 +0900

    feat(user): create interface user

--- Portfolio_maker_front/src/models/dummy/index.ts
+++ Portfolio_maker_front/src/models/dummy/index.ts
@@ -1,16 +1,5 @@
-export default interface DummyDto {
+export default interface PortfolioUser {
   id?: number;
   userName?: string;
   password?: string;
-  // fullName?: string;
-  // birthYear?: number;
-  // birthMonth?: number;
-  // birthDate?: number;
-  // gender?: "F" | "M";
-  // countryCode?: "KR";
-  // addressFull?: string;
-  // addressSido?: string;
-  // addressGugun?: string;
-  // addressDong?: string;
-  // addressRi?: string;
 }
