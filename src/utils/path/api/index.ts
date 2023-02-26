35f0c4511fd398bda7dbedc007933f0cf20dd7db
Dhyunlee <skyblue5030@naver.com>
2023-02-27 00:07:09 +0900

    feat(path): Add api path

---
+++ src/utils/path/api/index.ts
@@ -0,0 +1,22 @@
+const API_PATH = {
+  // User
+  API_LOGIN: "/api/user/login",
+  API_SIGNUP: "/api/user",
+
+  // MyInfo
+  API_GET_USER_INFO: "/api/users/info",
+  API_UPDATE_USER_INFO: "/api/users/update",
+  API_CREATE_PROFILE_IMAGE: "/api/users/img",
+  API_UPLOAD_PROFILE_IMAGE: "/api/users/img/update",
+
+  // Projects
+  API_CREATE_PROJECT: "/api/projects",
+  API_GET_PROJECT: "/api/projects?user-id=",
+  API_PUT_PROJECT: "/api/projects/",
+  API_DELETE_PROJECT: "/api/projects/",
+}
+
+
+Object.freeze(API_PATH);
+
+export default API_PATH;
