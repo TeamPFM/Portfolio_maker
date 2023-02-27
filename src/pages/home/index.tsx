349ec8c35a8423b44530dfa51514a083014e7ece
soonsung <tnstjd120@gmail.com>
2023-02-27 16:46:54 +0900

    style(home): 여백 제거

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -4,7 +4,6 @@ import { useNavigate } from "react-router-dom";
 import MainButton from "@/styles/ui-components/styled-button";
 import Path from "@/utils/path/routes";
 
-
 const HomePage = () => {
   const navigate = useNavigate();
   const { LOGIN } = Path;
