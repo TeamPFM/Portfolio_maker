09ee21e1227a8506520c171b6a58ee2996dae351
Gihun <563710@naver.com>
2023-02-27 23:09:00 +0900

    feat(common):  GNB 일반화

--- src/components/common/GNB.tsx
+++ src/components/common/GNB.tsx
@@ -11,7 +11,7 @@ import token from "@/libs/token";
 const GNB = () => {
   const navigate = useNavigate();
 
-  const { HOME, LOGIN, MYINFO } = Path;
+  const { HOME, LOGIN, MYINFO, BOARD } = Path;
   const [isToggleMenu, setIsToggleMenu] = useState<Boolean>(false);
 
   const authToken = token.getToken("token");
@@ -38,58 +38,84 @@ const GNB = () => {
   };
 
   return (
-    <div className="fixed left-0 top-0 w-full h-20 flex align-middle justify-between px-5 drop-shadow z-10 bg-white border-b-2">
-      <H1 className="logo">
-        <Link to={HOME}>Portfolio Maker</Link>
-      </H1>
+    <>
+      {isToggleMenu && (
+        <article
+          className="fixed w-full right0 left-0 bottom-0 bg-black bg-opacity-20 top-20 z-10"
+          onClick={() => {
+            setIsToggleMenu(false);
+          }}
+        ></article>
+      )}
 
-      <nav>
-        <ul className="flex space-x-4 items-center h-full">
-          <li>
-            <Link to={HOME} className="hover:text-main transition-all">
-              게시판
-            </Link>
-          </li>
-          <li className="relative">
-            <button
-              className="flex justify-center items-center rounded-full bg-main border-2 border-main opacity-70 hover:opacity-100 transition-all"
-              onClick={() => {
-                authToken
-                  ? setIsToggleMenu(!isToggleMenu)
-                  : navigate(LOGIN, { replace: true });
-              }}
-            >
-              <FaUserCircle className="text-3xl text-white" />
-            </button>
+      <div className="fixed left-0 top-0 w-full h-20 flex align-middle justify-between px-5 drop-shadow z-10 bg-white border-b-2">
+        <H1 className="logo">
+          <Link
+            to={HOME}
+            onClick={() => {
+              setIsToggleMenu(false);
+            }}
+          >
+            Portfolio Maker
+          </Link>
+        </H1>
 
-            <motion.div
-              className={`flex flex-col absolute top-[100%] right-[35px] w-[120px] bg-indigo-100 text-gray-500 rounded-lg rounded-tr-none overflow-hidden transition-all`}
-              initial="exit"
-              animate={isToggleMenu ? "enter" : "exit"}
-              variants={toggleMenuAnimate}
-            >
+        <nav>
+          <ul className="flex space-x-4 items-center h-full">
+            <li>
               <Link
-                to={MYINFO}
-                className="px-3 py-2 border-b-[1px] border-white hover:text-white hover:bg-main transition-all"
-                onClick={() => setIsToggleMenu(!isToggleMenu)}
-              >
-                내 정보
-              </Link>
-              <Link
-                to={HOME}
-                className="px-3 py-2 hover:text-white hover:bg-main transition-all"
+                to={BOARD}
+                className="hover:text-main transition-all"
                 onClick={() => {
-                  const removeToken = token.removeToken("token");
-                  const getToken = token.getToken("token");
+                  setIsToggleMenu(false);
                 }}
               >
-                로그아웃
+                게시판
               </Link>
-            </motion.div>
-          </li>
-        </ul>
-      </nav>
-    </div>
+            </li>
+            <li className="relative">
+              <button
+                className="flex justify-center items-center rounded-full bg-main border-2 border-main opacity-70 hover:opacity-100 transition-all"
+                onClick={() => {
+                  !authToken
+                    ? navigate(LOGIN, { replace: true })
+                    : setIsToggleMenu(!isToggleMenu);
+                }}
+              >
+                <FaUserCircle className="text-3xl text-white" />
+              </button>
+
+              <motion.div
+                className={`flex flex-col absolute top-[100%] right-[35px] w-[120px] bg-indigo-100 text-gray-500 rounded-lg rounded-tr-none overflow-hidden transition-all`}
+                initial="exit"
+                animate={isToggleMenu ? "enter" : "exit"}
+                variants={toggleMenuAnimate}
+              >
+                <div
+                  className="px-3 py-2 border-b-[1px] border-white hover:text-white hover:bg-main transition-all"
+                  onClick={() => {
+                    navigate(MYINFO, { replace: true });
+                    setIsToggleMenu(!isToggleMenu);
+                  }}
+                >
+                  내 정보
+                </div>
+                <div
+                  className="px-3 py-2 hover:text-white hover:bg-main transition-all"
+                  onClick={() => {
+                    navigate(LOGIN, { replace: true });
+                    setIsToggleMenu(!isToggleMenu);
+                    token.removeToken("token");
+                  }}
+                >
+                  로그아웃
+                </div>
+              </motion.div>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </>
   );
 };
 
