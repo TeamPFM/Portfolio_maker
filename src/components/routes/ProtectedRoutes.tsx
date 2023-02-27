59ead722855866a4899f1cdbaad47389270be5fd
soonsung <tnstjd120@gmail.com>
2023-02-27 22:09:48 +0900

    fix(routes): Modify - ProtectedRoutes

--- src/components/routes/ProtectedRoutes.tsx
+++ src/components/routes/ProtectedRoutes.tsx
@@ -4,16 +4,9 @@ import MyInfoPage from "@/pages/myinfo";
 import Path from "@/utils/path/routes";
 import { Navigate, Route, Routes } from "react-router-dom";
 import ResumePage from "@/pages/resume";
-
+import BoardPage from "@/pages/board";
 const ProtectedRoutes = () => {
-  const {
-    HOME,
-    LOGIN,
-    SIGNUP,
-    WRITE,
-    MYINFO,
-    RESUME,
-  } = Path;
+  const { HOME, LOGIN, SIGNUP, WRITE, MYINFO, RESUME, BOARD } = Path;
 
   return (
     // 로그인 되었을떄 보이는 페이지
@@ -21,6 +14,7 @@ const ProtectedRoutes = () => {
       <Route path={HOME} element={<HomePage />} />
       <Route path={MYINFO} element={<MyInfoPage />} />
       <Route path={WRITE} element={<WritePage />} />
+      <Route path={BOARD} element={<BoardPage />} />
       {/* Resume CRUD */}
       <Route path={RESUME} element={<ResumePage />} />
       {/*  */}
