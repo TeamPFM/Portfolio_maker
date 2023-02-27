56fd1f4db8bdcf5d5bdb779333e06a2d7da79140
soonsung <tnstjd120@gmail.com>
2023-02-27 16:47:58 +0900

    fix(myinfo): Modify - contentType / Add - API_PATH

--- src/pages/myinfo/index.tsx
+++ src/pages/myinfo/index.tsx
@@ -3,10 +3,9 @@ import { UserInfoGetResponse } from "@/models/myinfo";
 import SideMenu from "@/components/myinfo/SideMenu";
 import InfoContent from "@/components/myinfo/InfoContent";
 import api from "@/libs/axios/api";
+import API_PATH from "@/utils/path/api";
 
 export interface MyInfoProps {
-  contentType: string;
-  setContentType: Dispatch<React.SetStateAction<string>>;
   userInfo: UserInfoGetResponse;
   setUserInfo: Dispatch<React.SetStateAction<UserInfoGetResponse>>;
 }
@@ -22,7 +21,7 @@ const MyInfoPage = () => {
     imagePath: "",
     imageName: "",
   });
-  const [contentType, setContentType] = useState<string>("내 정보");
+  const { API_GET_USER_INFO } = API_PATH;
 
   useEffect(() => {
     getUserInfo();
@@ -30,10 +29,9 @@ const MyInfoPage = () => {
 
   const getUserInfo = async () => {
     await api
-      .get("api/users/info")
+      .get(API_GET_USER_INFO)
       .then((res) => {
         setUserInfo(res.data);
-        console.log(userInfo);
       })
       .catch((error) => console.log(error));
   };
@@ -41,18 +39,8 @@ const MyInfoPage = () => {
   return (
     <div className="w-full h-full p-5 bg-gray-50">
       <div className="flex w-full h-full max-w-[1400px] mx-auto">
-        <SideMenu
-          contentType={contentType}
-          setContentType={setContentType}
-          userInfo={userInfo}
-          setUserInfo={setUserInfo}
-        />
-        <InfoContent
-          contentType={contentType}
-          setContentType={setContentType}
-          userInfo={userInfo}
-          setUserInfo={setUserInfo}
-        />
+        <SideMenu userInfo={userInfo} setUserInfo={setUserInfo} />
+        <InfoContent userInfo={userInfo} setUserInfo={setUserInfo} />
       </div>
     </div>
   );
