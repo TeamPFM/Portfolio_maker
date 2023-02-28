6d626bdf3673bc1755a0cb00fb03ff0521f06176
soonsung <tnstjd120@gmail.com>
2023-02-28 20:15:55 +0900

    feat(models): Add - board interface

---
+++ src/models/board/detail/index.ts
@@ -0,0 +1,30 @@
+import { UserInfoGetResponse } from "@/models/myinfo";
+
+export interface CommentProps {
+  id: number;
+  users: any;
+  content: string;
+  createdAt: string;
+  updatedAt: string;
+  deletedAt: string | null;
+}
+
+export interface BoardInfoGetResponse {
+  id: number;
+  title: string;
+  content: string;
+  createdAt: string;
+  updatedAt: string;
+  deletedAt: string | null;
+  comments: CommentProps[];
+}
+
+export interface CommentCreateRequest {
+  boardId: string;
+  content: string | null;
+}
+
+export interface StatusResponse {
+  status: number;
+  success: boolean;
+}
