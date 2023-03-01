ed88421992d09484ac225e9951755900904d57ba
Gihun <563710@naver.com>
2023-03-02 02:36:09 +0900

    style(board): Add link

--- src/pages/board/index.tsx
+++ src/pages/board/index.tsx
@@ -6,7 +6,7 @@ import { useParams } from "react-router-dom";
 const BoardPage = () => {
   const { id } = useParams();
   return (
-    <div className="w-full h-full min-h-screen flex flex-col items-center justify-start pt-8">
+    <div className="w-full h-full min-h-screen flex flex-col items-center justify-start pt-6">
       {id ? <BoardDetail boardId={id} /> : <BoardMain />}
     </div>
   );
